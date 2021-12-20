7d0a43fac3fd1d1c5ca14e35dcd4940e42fd0dc2
yamare1 <70721412+yamare1@users.noreply.github.com>
2021-12-19 18:40:17 -0800

    Add files via upload

---
+++ build/precache-manifest.443b726b99cc50a6e61b0729537b7263.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b024cbe86e2a90e7cf490efd80d4c2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f0d20a415a0dd2a6d81",
+    "url": "/static/css/main.cd4ab470.chunk.css"
+  },
+  {
+    "revision": "99a003fd7db0334ecfd3",
+    "url": "/static/js/2.c082dfc9.chunk.js"
+  },
+  {
+    "revision": "9f0d20a415a0dd2a6d81",
+    "url": "/static/js/main.fadfc07b.chunk.js"
+  },
+  {
+    "revision": "598c09683ea71b68fc7a",
+    "url": "/static/js/runtime-main.f24ab76e.js"
+  }
+]);
